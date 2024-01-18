13508db5bcdd59a56dcd62c3790c56b7abe620f0
denis_danilov <denchik199819981998@gmail.com>
2024-01-18 15:06:27 +0400

    Заменил if-else на тернарный опреатор

--- app/src/main/java/hexlet/code/Games/Even.java
+++ app/src/main/java/hexlet/code/Games/Even.java
@@ -4,21 +4,16 @@ import hexlet.code.Engine;
 
 import java.util.Random;
 
-
 public class Even {
     public static void even() {
         String task = "Answer 'yes' if the number is even, otherwise answer 'no'.";
         String [] questions = new String[3];
         String [] results = new String[3];
-        for (int i = 0; i < 3; i = i + 1) {
+        for (int i = 0; i < 3; i++) {
             Random random = new Random();
             int random1 = random.nextInt(100);
             questions[i] = String.valueOf(random1);
-            if (random1 % 2 == 0) {
-                results[i] = "yes";
-            } else {
-               results[i] = "no";
-            }
+            String answer = (random1 % 2 == 0) ? (results[i] = "yes") : (results[i] = "no");
         }
         Engine.engine(questions, results, task);
     }

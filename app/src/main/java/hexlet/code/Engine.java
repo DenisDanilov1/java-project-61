2174f1c65666fa1c1d61a4672c0339218c7c8bea
denis_danilov <denchik199819981998@gmail.com>
2024-01-19 18:22:22 +0400

    Написал логику для игр

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,28 +4,26 @@ import java.util.Scanner;
 
 public class Engine {
 
-    public static void engine(String[] questions, String[] results, String task) {
+    public static final int ROUNDS = 3;
+    public static void engine(String[][] data, String task) {
         Scanner sc = new Scanner(System.in);
         System.out.println("May I have your name?");
         String userName = sc.next();
         System.out.println("Hello, " + userName + "!");
         System.out.println(task);
-        for (int i = 0; i < 3;) {
-            System.out.println("Question: " + questions[i]);
-            Scanner scan = new Scanner(System.in);
-            var userNumber = scan.next();
-            System.out.println("Your answer: " + userNumber);
-            if (userNumber.equals(String.valueOf(results[i]))) {
+        for (String[] arr : data) {
+                System.out.println("Question: " + arr[0]);
+                Scanner scan = new Scanner(System.in);
+                var userNumber = scan.next();
+                System.out.println("Your answer: " + userNumber);
+            if (userNumber.equals(arr[1])) {
                 System.out.println("Correct!");
-                if (i == 2){
-                    System.out.println("Congratulations, " + userName + "!");
-                }
-                    i++;
             } else {
-                System.out.println("'" + userNumber + "' is wrong answer ;(. Correct answer was '" + results[i] + "'.");
+                System.out.println("'" + userNumber + "' is wrong answer ;(. Correct answer was '" + arr[1] + "'.");
                 System.out.println("Let's try again, " + userName + "!");
-                i = 3;
+                return;
             }
         }
+        System.out.println("Congratulations, " + userName + "!");
     }
 }

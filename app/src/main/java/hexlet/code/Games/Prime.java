80c23a2bd850faafd27a5b27802383fe341ae35c
denis_danilov <denchik199819981998@gmail.com>
2024-01-19 20:36:28 +0400

    Переписал код для простого числа

--- app/src/main/java/hexlet/code/Games/Prime.java
+++ app/src/main/java/hexlet/code/Games/Prime.java
@@ -1,32 +1,40 @@
 package hexlet.code.Games;
 
 import hexlet.code.Engine;
+import hexlet.code.Utils;
 
 
-import java.math.BigInteger;
-import java.util.Random;
+
 
 public class Prime {
+    public static final String task = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+    public static final int MIN = 1;
+    public static final int MAX = 100;
+
     public static void prime() {
-        String task = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-        String[] questions = new String[3];
-        String[] results = new String[3];
-        for (int i = 0; i < 3; i++) {
-            Random random = new Random();
-            int random1 = random.nextInt(100);
-            String str1 = String.valueOf(random1);
-            questions[i] = str1;
-            if (random1 < 2) {
-                results[i] = "no";
-            } else {
-                class IsPrime {
-                    public boolean isPrime(int number) {
-                        BigInteger bigInt = BigInteger.valueOf(number);
-                        return bigInt.isProbablePrime(100);
-                    }
-                }
-            }
-            Engine.engine(questions, results, task);
+        String[][] data = new String[Engine.ROUNDS][2];
+        for (int i = 0; i < Engine.ROUNDS; i++) {
+            int random = Utils.generateNumber(MIN, MAX);
+            String question = Integer.toString(random);
+            String correctAnswer =  isPrime(random) ? "yes" : "no";
+
+
+            data[i][0] = question;
+            data[i][1] = correctAnswer;
+
         }
+        Engine.engine(data, task);
+    }
+
+    private static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }

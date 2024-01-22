3365001e4b0cdeaaa79a586cec41523778f92e7e
denis_danilov <denchik199819981998@gmail.com>
2024-01-22 17:10:34 +0400

    Добавил пробелы

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,7 +1,9 @@
 package hexlet.code;
 
 import java.util.Scanner;
+
 public class Cli {
+
     public static void salutation() {
         Scanner sc = new Scanner(System.in);
         System.out.println("May I have your name?");
